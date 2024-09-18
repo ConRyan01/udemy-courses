8641561dbc7c177aaf10cb8e7e053337609ea0c2
Connor Ryan <101826079+ConRyan01@users.noreply.github.com>
2024-09-18 11:50:40 -0400

    added error handling to NATO alphabet project

--- 100 days of code - python bootcamp/intermediate/NATO_alphabet/main.py
+++ 100 days of code - python bootcamp/intermediate/NATO_alphabet/main.py
@@ -2,6 +2,15 @@ import pandas
 
 nato_alpha = {row.letter:row.code for (index,row) in pandas.read_csv('nato_phonetic_alphabet.csv').iterrows()}
 
-input_word = input('Enter a word to translate: ').upper()
-coded_word = " ".join([nato_alpha[letter] for letter in input_word])
-print(coded_word)
+def generate_phonetic():
+    input_word = input('Enter a word to translate: ').upper()
+
+    try:
+        coded_word = " ".join([nato_alpha[letter] for letter in input_word])
+    except KeyError:
+        print('Only letters of the alphabet please.')
+        generate_phonetic()
+    else:
+        print(coded_word)
+
+generate_phonetic()
